74ad9326e436d64c93b27a823c19a640f42e45ce
Eldar Muminov <emuminov@z4r2p2.42lyon.fr>
2024-03-21 18:08:18 +0100

    feat: updated test to take string as an argument

--- lexer/_tests.c
+++ lexer/_tests.c
@@ -6,19 +6,23 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 13:29:46 by emuminov          #+#    #+#             */
-/*   Updated: 2024/03/21 15:27:52 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/03/21 17:51:30 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lexer.h"
+#include <stdio.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_token	*lst;
 
-	// echo 1 < 0 hello 1 >> 0 > 0 | 1 "''" 0 'hello' 0 "''" 1 | 1 | 1 asdasda 1 zxc 1 << 0 qw 0 | 0 a 1 "QU..A" 1 'small quote' 0
-	lst = lexer("\"\"echo\"\" <\"hello\"  >>>| \"\"\"''\"'hello'\"''\"\"\" | | asdasda \
-zxc <<qw|a \"QUOTED AGAIN A\" 'small quote'''");
+	if (argc != 2)
+	{
+		printf("Enter proper command\n");
+		return (EXIT_FAILURE);
+	}
+	lst = lexer(argv[1]);
 	token_list_print(lst);
 	token_list_free(lst);
 }
