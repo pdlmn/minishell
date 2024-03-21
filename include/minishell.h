478b062a1558403540c212e05a750a737ba3a5ca
Eldar Muminov <emuminov@z4r2p2.42lyon.fr>
2024-03-21 14:00:18 +0100

    feat: normalized minishell.h and added prototypes for lexer functions

--- include/minishell.h
+++ include/minishell.h
@@ -6,39 +6,44 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 14:34:14 by emuminov          #+#    #+#             */
-/*   Updated: 2024/03/20 15:57:23 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/03/21 13:31:56 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 
-enum	e_token
+enum				e_token
 {
 	WORD,
 	OPERATOR,
 };
 
-/* 
+/*
  * IN_REDIR         = <
  * OUT_REDIR        = >
  * HEREDOC          = <<
  * OUT_REDIR_APPEND = >>
  * PIPE             = |
  * */
-enum	e_operator
+enum				e_operator
 {
 	NOT_OPERATOR,
-	IN_REDIR,        
-	OUT_REDIR,       
-	HEREDOC,         
+	IN_REDIR,
+	OUT_REDIR,
+	HEREDOC,
 	OUT_REDIR_APPEND,
-	PIPE,            
+	PIPE,
 };
 
-typedef struct	s_token
+typedef struct s_token
 {
 	struct s_token	*next;
 	char			*content;
 	enum e_token	type;
 	enum e_operator	op_type;
-}				t_token;
+	int				space_after;
+}					t_token;
+
+t_token				*lexer(char *input);
+void				token_list_free(t_token *lst);
+void				token_list_print(t_token *lst);
