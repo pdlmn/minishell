c3e5466489a23157073e841e189d65546467d0a2
Eldar Muminov <emuminov@z4r2p2.42lyon.fr>
2024-03-21 18:07:50 +0100

    refactor: added include for new function

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:38:46 by emuminov          #+#    #+#             */
-/*   Updated: 2024/03/21 13:47:23 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/03/21 18:00:37 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,5 @@ t_token			*token_create(enum e_token type, enum e_operator op,
 					char *content, int space_after);
 void			token_list_append(t_tlist *lst, t_token *token);
 void			token_list_free(t_token *lst);
+void			token_free(t_token *lst);
 void			token_list_print(t_token *lst);
