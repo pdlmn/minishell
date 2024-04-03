0258619b4c1b8cc0e2eb93c63aad81b490d746fc
Eldar Muminov <emuminov@z1r2p2.42lyon.fr>
2024-04-03 02:59:21 +0200

    feat: updated expansion.h with functions and updated .gitignore

--- include/expansion.h
+++ include/expansion.h
@@ -6,11 +6,11 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 05:17:12 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/03 00:24:41 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/03 02:32:00 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char	*expand_variable(t_ht_table *ht, char *word);
 char	*unquote(char *word, int quote);
+char	*expand_word(t_ht_table *ht, char *word);
