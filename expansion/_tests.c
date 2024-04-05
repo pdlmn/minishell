de33fe31de8a4b6151a7803ccc196ec4c30914a2
Eldar Muminov <emuminov@z4r6p3.42lyon.fr>
2024-04-05 19:54:34 +0200

    test: added more tests and added asserts to existing ones

--- expansion/_tests.c
+++ expansion/_tests.c
@@ -6,16 +6,18 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 12:23:17 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/05 19:03:10 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/05 19:53:13 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/expansion.h"
+#include <assert.h>
 
-#define test_expansion(ht, str) \
+#define test_expansion(ht, str, should_be) \
 { \
 	char *test = expand_word(ht, str); \
 	printf("%s\n", test); \
+	assert(ft_strcmp(test, should_be) == 0); \
 	free(test); \
 }
 
@@ -25,14 +27,17 @@ int	main(void)
 
 	ht = ht_new(100);
 	ht_set(ht, "ASD", "123");
-	test_expansion(ht, "$ASD");       // 123
-	test_expansion(ht, "Hello$ASD");  // Hello123
-	test_expansion(ht, "$ASDHello");  // {empty}
-	test_expansion(ht, "$ASD$ASD");   // 123123
-	test_expansion(ht, "$1");         // {empty}
-	test_expansion(ht, "$123a");      // 23a
-	test_expansion(ht, "\"$ASD\"");   // 123
-	test_expansion(ht, "'$ASD'$ASD"); // $ASD123
-	test_expansion(ht, "$\"A\"SD");   // $ASD
+	test_expansion(ht, "$ASD", "123");
+	test_expansion(ht, "Hello$ASD", "Hello123");
+	test_expansion(ht, "$ASDHello", "");
+	test_expansion(ht, "$ASD$ASD", "123123");
+	test_expansion(ht, "$1", "");
+	test_expansion(ht, "$123a", "23a");
+	test_expansion(ht, "\"$ASD\"", "123");
+	test_expansion(ht, "'$ASD'$ASD", "$ASD123");
+	test_expansion(ht, "$\"A\"SD", "ASD");
+	test_expansion(ht, "$\"ASD\"", "ASD");
+	test_expansion(ht, "\"$A\"SD", "SD");
+	test_expansion(ht, "$", "$");
 	ht_free_table(ht);
 }
