7e12b511ba3bf18c562711151b35b225d3da66e6
Eldar Muminov <emuminov@z1r8p2.42lyon.fr>
2024-04-09 17:46:16 +0200

    style: included normalized files and functions

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:38:46 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/09 15:05:55 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/09 17:45:56 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,17 @@ typedef struct s_tlist
 }				t_tlist;
 
 
+// utils.c
 enum e_operator	get_operator(char *str);
 enum e_token	get_type(char *str);
-char			*dup_quoted(char quote, int *i, char *input);
-char			*dup_until_next_space(int *i, char *input);
-char			*dup_operator(int *i, char *input);
+char			*ft_strset(char *s, char *set);
+
+// token_operations.c
 t_token			*token_create(char *content, int len, int space_after, enum e_quotes is_quoted);
 void			token_list_append(t_tlist *lst, t_token *token);
 void			token_list_free(t_token *lst);
 void			token_free(t_token *lst);
 void			token_list_print(t_token *lst);
-char			*ft_strset(char *s, char *set);
+
+// token_creation.c
+t_token			*token_create_from_input(char *input, enum e_quotes is_quoted);
