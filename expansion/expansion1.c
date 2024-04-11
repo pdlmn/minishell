5631d07339b17712129fe0420401c659eac5b993
Eldar Muminov <emuminov@z3r7p5.42lyon.fr>
2024-04-11 16:39:57 +0200

    refactor: changed types of expansion function to t_tlist

--- expansion/expansion1.c
+++ expansion/expansion1.c
@@ -6,11 +6,13 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 05:16:56 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/10 20:17:38 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:38:21 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/expansion.h"
+
+// input -> tokenization -> checking for errors -> expansion -> command_table
 
 /* 1. Replace non singly-quoted $ values.
  * 2. Join all strings.
@@ -35,9 +37,8 @@ t_token	*token_find_prev(t_token *lst, t_token *t)
 	return (prev);
 }
 
-t_token	*expand_sigil(t_ht_table *ht, t_token **lst, t_token *sigil_token)
+t_token	*expand_sigil(t_tlist *lst, t_ht_table *ht, t_token *sigil_token)
 {
-	t_token	*prev;
 	char	*val;
 	char	*copied_val;
 	t_token	*res;
@@ -45,7 +46,6 @@ t_token	*expand_sigil(t_ht_table *ht, t_token **lst, t_token *sigil_token)
 	if (!sigil_token->space_after && sigil_token->next
 		&& sigil_token->next->op_type == NOT_OPERATOR)
 	{
-		prev = token_find_prev(*lst, sigil_token);
 		if (sigil_token->next->type == WORD)
 		{
 			// delete SIGIL, replace contents of the next word with cloned ht value
@@ -56,22 +56,22 @@ t_token	*expand_sigil(t_ht_table *ht, t_token **lst, t_token *sigil_token)
 			free(sigil_token->next->content);
 			sigil_token->next->content = copied_val;
 			sigil_token->next->len = ft_strlen(copied_val);
-			if (prev)
-				prev->next = sigil_token->next;
+			if (sigil_token->prev)
+				sigil_token->prev->next = sigil_token->next;
 			else
-				*lst = sigil_token->next;
+				lst->head = sigil_token->next;
 			res = sigil_token->next;
 			token_free(sigil_token);
 			return (res);
 		}
-		else if ((sigil_token->next->type == QUOTE || sigil_token->next->type == DQUOTE)
+		else if ((sigil_token->next->type == SQUOTE || sigil_token->next->type == DQUOTE)
 				&& sigil_token->is_quoted == NOT_QUOTED)
 		{
 			// delete SIGIL
-			if (prev)
-				prev->next = sigil_token->next;
+			if (sigil_token->prev)
+				sigil_token->prev->next = sigil_token->next;
 			else
-				*lst = sigil_token->next;
+				lst->head = sigil_token->next;
 			res = sigil_token->next;
 			token_free(sigil_token);
 			return (res);
@@ -94,17 +94,38 @@ t_token	*expand_sigil(t_ht_table *ht, t_token **lst, t_token *sigil_token)
 	return (sigil_token);
 }
 
+t_token	*expand_sigils(t_ht_table *ht, t_tlist *lst)
+{
+	t_token	*curr;
+
+	curr = lst->head;
+	while (curr)
+	{
+		if (curr->type == SIGIL)
+		{
+			curr = expand_sigil(lst, ht, curr);
+			if (!curr)
+				return (NULL);
+		}
+		else
+			curr = curr->next;
+	}
+	return (curr);
+}
+
 t_token	*merge_word_tokens(t_token *t1, t_token *t2)
 {
 	char	*s;
 	int		len;
 
 	len = t1->len + t2->len;
-	s = malloc(sizeof(char) * (len + 1));
+	s = malloc(sizeof(char) * (len + t1->space_after + 1));
 	if (!s)
 		return (NULL);
 	ft_memcpy(s, t1->content, sizeof(char) * t1->len);
-	ft_memcpy(&s[t1->len], t2->content, sizeof(char) * t2->len);
+	if (t1->space_after)
+		s[t1->len] = ' ';
+	ft_memcpy(&s[t1->len + t1->space_after], t2->content, sizeof(char) * t2->len);
 	s[t2->len] = '\0';
 	free(t1->content);
 	t1->content = s;
@@ -134,30 +155,11 @@ t_token	*join_quotes_content(t_token *quote_token)
 	return (curr);
 }
 
-t_token	*expand_sigils(t_ht_table *ht, t_token **lst)
+t_token	*expand_quotes(t_tlist *lst)
 {
 	t_token	*curr;
 
-	curr = *lst;
-	while (curr)
-	{
-		if (curr->type == SIGIL)
-		{
-			curr = expand_sigil(ht, lst, curr);
-			if (!curr)
-				return (NULL);
-		}
-		else
-			curr = curr->next;
-	}
-	return (curr);
-}
-
-t_token	*expand_quotes(t_token *lst)
-{
-	t_token	*curr;
-
-	curr = lst;
+	curr = lst->head;
 	while (curr)
 	{
 		if (curr->is_quoted == START_QUOTE)
@@ -171,21 +173,19 @@ t_token	*expand_quotes(t_token *lst)
 	return (curr);
 }
 
-t_token	*remove_quotes(t_token **lst)
+t_token	*remove_quotes(t_tlist *lst)
 {
 	t_token	*curr;
-	t_token	*prev;
 
-	curr = *lst;
+	curr = lst->head;
 	while (curr)
 	{
-		if (curr->type == QUOTE || curr->type == DQUOTE)
+		if (curr->type == SQUOTE || curr->type == DQUOTE)
 		{
-			prev = token_find_prev(*lst, curr);
-			if (prev)
-				prev->next = curr->next;
+			if (curr->prev)
+				curr->prev->next = curr->next;
 			else
-				*lst = curr->next;
+				lst->head = curr->next;
 			token_free(curr);
 		}
 		curr = curr->next;
@@ -207,11 +207,11 @@ t_token	*join_unspaced_words(t_token *t)
 	}
 }
 
-t_token	*expansion(t_ht_table *ht, t_token **lst)
+t_tlist	*expansion(t_tlist *lst, t_ht_table *ht)
 {
 	expand_sigils(ht, lst);
-	expand_quotes(*lst);
+	expand_quotes(lst);
 	remove_quotes(lst);
 	// join_unspaced_words
-	return (*lst);
+	return (lst);
 }
