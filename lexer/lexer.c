b9e1c579c295fcf48dfd42abc850e6c0e860c778
Eldar Muminov <emuminov@z3r7p5.42lyon.fr>
2024-04-11 15:59:53 +0200

    refactor: made set_quote function a bit cleaner

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,21 +6,21 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:09:12 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/11 15:33:23 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:59:36 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lexer.h"
 
-void	set_is_quoted(t_tlist *lst, enum e_quotes *is_quoted)
+void	set_is_quoted(t_token *t, enum e_quotes *is_quoted)
 {
-	if (lst->tail->type == SQUOTE && *is_quoted == NOT_QUOTED)
+	if (t->type == SQUOTE && *is_quoted == NOT_QUOTED)
 		*is_quoted = SQUOTED;
-	else if (lst->tail->type == SQUOTE && *is_quoted == SQUOTED)
+	else if (t->type == SQUOTE && *is_quoted == SQUOTED)
 		*is_quoted = NOT_QUOTED;
-	if (lst->tail->type == DQUOTE && *is_quoted == NOT_QUOTED)
+	if (t->type == DQUOTE && *is_quoted == NOT_QUOTED)
 		*is_quoted = DQUOTED;
-	else if (lst->tail->type == DQUOTE && *is_quoted == DQUOTED)
+	else if (t->type == DQUOTE && *is_quoted == DQUOTED)
 		*is_quoted = NOT_QUOTED;
 }
 
@@ -48,7 +48,7 @@ t_token	*lexer(char *input)
 			return (token_list_free(lst.head), NULL);
 		token_list_append(&lst, t);
 		i += lst.tail->len;
-		set_is_quoted(&lst, &is_quoted);
+		set_is_quoted(t, &is_quoted);
 	}
 	return (lst.head);
 }
