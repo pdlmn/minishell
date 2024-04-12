0d3db05698286385a1e48e6d5b29a4589df641e0
Eldar Muminov <emuminov@z1r10p5.42lyon.fr>
2024-04-12 17:34:35 +0200

    feat: added new utils functions for determining the end of valid variable name and deteleted not used terminate function

--- include/lexer.h
+++ include/lexer.h
@@ -6,16 +6,18 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:38:46 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/11 20:13:30 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/12 17:33:15 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // utils.c
-enum e_operator	get_operator(char *str);
-enum e_token	get_type(char *str, enum e_quotes is_quoted);
+enum e_operator	get_operator(char *str, enum e_quotes is_quoted);
+enum e_token	get_type(char *str, enum e_quotes is_quoted, enum e_operator op);
 char			*ft_strset(char *s, char *set);
+inline int		is_valid_variable_char(int c);
+int		 		find_end_of_variable_name(char *s);
 
 // token_operations.c
 void			token_list_append(t_tlist *lst, t_token *token);
--- lexer/utils.c
+++ lexer/utils.c
@@ -6,29 +6,32 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 13:34:37 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/11 20:21:51 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/12 17:33:46 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lexer.h"
 
 /* Determine if the first 1 or 2 characters of the string are operators */
-enum e_operator	get_operator(char *str)
+enum e_operator	get_operator(char *str, enum e_quotes is_quoted)
 {
-	if (ft_strncmp(">>", str, 2) == 0)
-		return (OUT_REDIR_APPEND);
-	else if (ft_strncmp("<<", str, 2) == 0)
-		return (HEREDOC);
-	else if (ft_strncmp(">", str, 1) == 0)
-		return (OUT_REDIR);
-	else if (ft_strncmp("<", str, 1) == 0)
-		return (IN_REDIR);
-	else if (ft_strncmp("|", str, 1) == 0)
-		return (PIPE);
+	if (is_quoted == NOT_QUOTED)
+	{
+		if (ft_strncmp(">>", str, 2) == 0)
+			return (OUT_REDIR_APPEND);
+		else if (ft_strncmp("<<", str, 2) == 0)
+			return (HEREDOC);
+		else if (ft_strncmp(">", str, 1) == 0)
+			return (OUT_REDIR);
+		else if (ft_strncmp("<", str, 1) == 0)
+			return (IN_REDIR);
+		else if (ft_strncmp("|", str, 1) == 0)
+			return (PIPE);
+	}
 	return (NOT_OPERATOR);
 }
 
-enum e_token	get_type(char *str, enum e_quotes is_quoted)
+enum e_token	get_type(char *str, enum e_quotes is_quoted, enum e_operator op)
 {
 	if (str[0] == '\'' && is_quoted != DQUOTED)
 		return (SQUOTE);
@@ -38,8 +41,12 @@ enum e_token	get_type(char *str, enum e_quotes is_quoted)
 		return (TILDE);
 	else if (str[0] == '$' && (is_quoted == NOT_QUOTED || is_quoted == DQUOTED))
 		return (SIGIL);
-	else if (get_operator(str) != NOT_OPERATOR && is_quoted == NOT_QUOTED)
+	else if (op != NOT_OPERATOR && is_quoted == NOT_QUOTED)
 		return (OPERATOR);
+	else if (ft_isdigit(str[0]))
+		return (DIGIT);
+	else if (!ft_isalpha(str[0]) && str[0] != ' ')
+		return (OTHER);
 	return (WORD);
 }
 
@@ -59,13 +66,22 @@ char	*ft_strset(char *s, char *set)
 	return (&s[i]);
 }
 
-void	terminate(t_tlist *lst, char *str_to_free, char *message)
+inline int	is_valid_variable_char(int c)
 {
-	if (str_to_free)
-		free(str_to_free);
-	if (lst)
-		token_list_free(lst);
-	if (message)
-		ft_putstr_fd(message, STDERR_FILENO);
-	exit(EXIT_FAILURE);
+	return (!ft_isalnum(c) && c != '_');
 }
+
+int 	find_end_of_variable_name(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (is_valid_variable_char(s[i]))
+			return (&s[i] - s);
+		i++;
+	}
+	return (&s[i] - s);
+}
+
