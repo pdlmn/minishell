a7d3e8fa33f60d387d978343c72ba7ea66802aad
Eldar Muminov <emuminov@z4r7p7.42lyon.fr>
2024-04-19 15:19:04 +0200

    feat: implemented delimeter detection, made post-expansion tokens to contain more accurate information, change quoted type of expanded tokens to not_quoted

--- expansion/heredoc_find_delimeters.c
+++ expansion/heredoc_find_delimeters.c
@@ -6,34 +6,46 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:04:44 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/18 20:37:36 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/19 14:47:34 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static t_token	*convert_to_delimeter(t_token *t)
+static int	type_delim(t_token *t)
 {
 	t_token			*curr;
-	t_token			*delim;
 	enum e_token	type;
 
 	curr = t;
 	type = DELIM;
-	delim = NULL;
+	while (curr && (!curr->space_after || curr->is_quoted != START_QUOTE))
+	{
+		if (curr->type == START_QUOTE)
+		{
+			type = QDELIM;
+			break ;
+		}
+		curr = curr->next;
+	}
+	return (type);
+}
+
+static t_token	*convert_to_delimeter(t_token *t)
+{
+	t_token				*curr;
+	const enum e_token	type = type_delim(t);
+
+	curr = t;
 	while (curr)
 	{
-		if (curr->is_quoted == START_QUOTE)
-			type = QDELIM;
-		if (delim && curr->type != SQUOTE && curr->type != DQUOTE && curr->space_after)
-			break ;
-		else if (!delim && curr->type != SQUOTE && curr->type != DQUOTE)
-			delim = curr;
-		else
-			curr = curr->next;
+		if (curr->type != DQUOTE && curr->type != SQUOTE)
+			curr->type = type;
+		if (curr->space_after && (curr->type != DQUOTE && curr->type != SQUOTE))
+			break;
+		curr = curr->next;
 	}
-	delim->type = type;
-	return (delim);
+	return (curr);
 }
 
 t_tlist	*heredoc_find_delimeters(t_minishell *sh)
@@ -44,8 +56,9 @@ t_tlist	*heredoc_find_delimeters(t_minishell *sh)
 	while (curr)
 	{
 		if (curr->op_type == HEREDOC && curr->next)
-			convert_to_delimeter(curr->next);
-		curr = curr->next;
+			curr = convert_to_delimeter(curr->next);
+		else
+			curr = curr->next;
 	}
 	return (&sh->lst);
 }
