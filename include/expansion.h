bd1fbf274a9492fbd20717835f5aaa8c2ba1f739
Eldar Muminov <emuminov@z4r7p7.42lyon.fr>
2024-04-19 20:49:23 +0200

    feat: converted some function names to verbs, started to work on the expansion inside of heredoc

--- include/expansion.h
+++ include/expansion.h
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 05:17:12 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/19 15:25:39 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/19 18:50:33 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_tlist	*expand_quotes(t_tlist *lst);
 t_tlist	*remove_quotes(t_tlist *lst);
 
 // expand_variables.c
-t_tlist	*expand_variables(t_minishell *sh);
+t_tlist	*expand_variables(t_minishell *sh, t_tlist *lst);
 
 // utils.c
 t_tlist	*join_unspaced_words(t_tlist *lst);
