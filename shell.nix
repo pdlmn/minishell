047cbdc67d9b5cf87625b7ece7120dc78e9af2de
pdlmn <jseldardw@gmail.com>
2024-06-08 22:32:22 +0200

    misc: updated shell.nix again

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 with import <nixpkgs> {};
-(mkShell.override { stdenv = llvmPackages_12.stdenv; }) {
-  nativeBuildInputs = with pkgs; [ clang-tools readline ];
+(mkShell.override { stdenv = clangStdenv; }) {
+  nativeBuildInputs = with pkgs; [ clang-tools readline llvmPackages_12.libllvm ];
 }
