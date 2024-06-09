0f7e2570f522f7bd933de6f7291d60990a208f7f
pdlmn <jseldardw@gmail.com>
2024-06-09 12:54:03 +0200

    fix: better version of ctrl + c

--- shell/handle_signal.c
+++ shell/handle_signal.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:35:45 by emuminov          #+#    #+#             */
-/*   Updated: 2024/06/07 18:37:02 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/06/09 12:53:41 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,10 @@ void	interactive_signal_handler(int signal)
 {
 	(void)signal;
 	set_or_get_exit_status(SET, 130);
-	ioctl(STDIN_FILENO, TIOCSTI, "\n");
+	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
+	rl_redisplay();
 }
 
 void	init_interacrive_signal_handlers(void)
